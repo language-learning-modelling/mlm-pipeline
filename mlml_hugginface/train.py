a1be11956395b920ff5872426c8b81c691c2b6d5
bernardo <bernardo.stearns@gmail.com>
2024-09-16 17:06:52 +0100

    keeping only metadata as extra feature

--- mlml_hugginface/train.py
+++ mlml_hugginface/train.py
@@ -67,14 +67,12 @@ class TrainerConfig:
 class CustomDataCollator(DataCollatorForLanguageModeling):
     def __call__(self, features):
         # Separate the tokenized input and the metadata
-        ids = [feature.pop('id') for feature in features]
         metadata = [feature.pop('text_metadata') for feature in features]
         
         # Call the parent collator to handle tokenized input
         batch = super().__call__(features)
         
         # Add the id and metadata back to the batch
-        batch['id'] = ids
         batch['metadata'] = metadata
         
         return batch
