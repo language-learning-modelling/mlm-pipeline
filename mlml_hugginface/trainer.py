bad846363ae867461bb248c079cbce147c573157
bernardo <bernardo.stearns@gmail.com>
2024-09-17 12:45:51 +0100

    removing state_dict variable

--- mlml_hugginface/trainer.py
+++ mlml_hugginface/trainer.py
@@ -22,7 +22,7 @@ class CustomTrainer(Trainer):
                 trainer_state_fp = os.path.join(resume_from_checkpoint, "trainer_state.json")
                 with open(trainer_state_fp) as inpf:
                     self.state = json.load(inpf)
-            print(state_dict)
+            print(f"{self.state}")
             print(f"Resuming at global_step {self.state.global_step}")
         super().train(resume_from_checkpoint=resume_from_checkpoint, **kwargs)
 
